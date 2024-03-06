437a9d7a54862dd1ec88c1051f994ca2b0ccf09f
EdissonFacundo <aquinoedissonfacundo@gmail.com>
2024-03-06 01:53:42 -0300

    config dependencias

--- Client/src/Components/Admin/Cards/Cards.jsx
+++ Client/src/Components/Admin/Cards/Cards.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import Cardslist from '../CardList/CardList';
 import styles from './Cards.module.css';
 const Cards = () => {
-	return (
-		<div className={styles.Cardscontainer}>
-			<h1 className={styles.Cardstitle}>Lista de Productos</h1>
-			<Cardslist />
-		</div>
-	);
+  return (
+    <div className={styles.Cardscontainer}>
+      <h1 className={styles.Cardstitle}>Lista de Productos</h1>
+      <Cardslist />
+    </div>
+  );
 };
 
 export default Cards;
